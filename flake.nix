cb8eb9a0d82f40105199031a40792b853f7313df
Chris Toph <toph@ryot.foo>
2025-11-01 00:08:18 -0400

    feat: update configuration for PMW3610 driver and improve flake for local build process

--- flake.nix
+++ flake.nix
@@ -1,43 +1,77 @@
 {
-  description = "Keyball39 ZMK Firmware Development Environment";
+  description = "Keyball39 ZMK Firmware";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
+      zmk-nix,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "keyball39-dev";
+    let
+      forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+    in
+    {
+      packages = forAllSystems (system: {
+        default = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+          name = "keyball39";
 
-          buildInputs = with pkgs; [
-            # Version control
-            git
-
-            # GitHub Actions local runner (requires Docker)
-            act
-
-            # Optional: If you want to build locally without Docker
-            # Uncomment these lines:
-            # python3
-            # python3Packages.west
-            # cmake
-            # ninja
-            # gcc-arm-embedded
-            # dtc
+          # Filter source files like the template does
+          src = nixpkgs.lib.sourceFilesBySuffices self [
+            ".board"
+            ".cmake"
+            ".conf"
+            ".defconfig"
+            ".dts"
+            ".dtsi"
+            ".json"
+            ".keymap"
+            ".overlay"
+            ".shield"
+            ".yml"
+            "_defconfig"
           ];
+
+          board = "nice_nano_v2";
+          # Builds keyball39_left and keyball39_right with nice!view display support
+          shield = "keyball39_%PART% nice_view_adapter nice_view";
+
+          # Hash of Zephyr dependencies from west.yml in config/
+          # The config parameter defaults to "config" which is where our west.yml lives
+          # Includes: ZMK main + zmk-pmw3610-driver for trackball support
+          zephyrDepsHash = "sha256-MjjLh+k6hIRGz3IbClbKPo6PAuRur4bURSTmys5R0+U=";
         };
-      }
-    );
+      });
+
+      # Convenience outputs for building and flashing
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/flash";
+        };
+
+        flash = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/flash";
+        };
+
+        update = zmk-nix.apps.${system}.update;
+      });
+
+      # Dev shell for local development
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          name = "keyball39-dev";
+          inputsFrom = [ self.packages.${system}.default ];
+        };
+      });
+    };
 }
